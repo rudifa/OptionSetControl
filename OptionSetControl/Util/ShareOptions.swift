c607b1b89184bf03d10dc19699a5af835c7909e0
Rudi Farkas <rudi.farkas@gmail.com>
2020-06-15 11:40:25 +0200

    add ShareOptions, ShareOptionsTests

---
+++ OptionSetControl/Util/ShareOptions.swift
@@ -0,0 +1,36 @@
+//
+//  ShareOptions.swift
+//  Stick-Print
+//
+//  Created by Rudolf Farkas on 15.06.20.
+//  Copyright © 2020 Eric PAJOT. All rights reserved.
+//
+
+import Foundation
+
+// see https://developer.apple.com/documentation/swift/optionset
+// see https://cocoacasts.com/how-to-work-with-bitmasks-in-swift/
+// see https://cocoacasts.com/how-to-create-a-custom-control-using-a-bitmask
+// see https://github.com/bartjacobs/HowToCreateACustomControlUsingABitmask
+
+struct ShareOptions: OptionSet {
+
+    init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+
+    init() {
+        rawValue = ShareOptions.all.rawValue
+    }
+
+    let rawValue: Int
+
+    static let email = ShareOptions(rawValue: 1 << 0)
+    static let addGroup = ShareOptions(rawValue: 1 << 1)
+    static let faceTime = ShareOptions(rawValue: 1 << 2)
+    static let booking = ShareOptions(rawValue: 1 << 3)
+    static let warning = ShareOptions(rawValue: 1 << 4)
+    static let lock = ShareOptions(rawValue: 1 << 5)
+
+    static let all: ShareOptions = [.email, .addGroup, .faceTime, .booking, .warning, .lock]
+}
---
+++ OptionSetControlTests/ShareOptionsTests.swift
@@ -0,0 +1,19 @@
+//
+//  ShareOptionsTests.swift
+//  Stick-PrintTests
+//
+//  Created by Rudolf Farkas on 15.06.20.
+//  Copyright © 2020 Eric PAJOT. All rights reserved.
+//
+
+import XCTest
+
+class ShareOptionsTests: XCTestCase {
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+
+    func test_ShareOptions() {
+        let opts = ShareOptions()
+    }
+}
